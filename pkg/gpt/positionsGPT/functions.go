4365cfbb5a26f82d44ac769e8cc4b3145c35eef2
Rosya-edwica <yunoshev.dev@gmail.com>
2023-09-28 17:49:48 +0300

    уменьшил количество запрашиваемых функций с 30 до 10

--- pkg/gpt/positionsGPT/functions.go
+++ pkg/gpt/positionsGPT/functions.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetFunctionsForPosition(name string) (functions []string, err error) {
-	question := fmt.Sprintf(`Составь список из 30 профессиональных функций профессии "%s". Пиши в строчку. Не используй нумерацию. В качестве разделителя используй знак ,`, name)
+	question := fmt.Sprintf(`Составь список из 10 профессиональных функций профессии "%s". Пиши в строчку. Не используй нумерацию. В качестве разделителя используй знак ,`, name)
 	answer, err := gpt.SendRequestToGPT(question)
 	functions = strings.Split(answer, ",")
 	if err != nil {
