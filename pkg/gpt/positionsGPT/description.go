f9a6474eb3dd8198aaa9d86422af3105273304a1
Rosya-edwica <yunoshev.dev@gmail.com>
2023-09-28 19:43:32 +0300

    сократил запрос для поиска описаний

--- pkg/gpt/positionsGPT/description.go
+++ pkg/gpt/positionsGPT/description.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetDescriptionForPosition(name string) (descr string, err error) {
-	question := fmt.Sprintf(`Составь подробное описание профессии "%s"`, name)
+	question := fmt.Sprintf(`Выступи в роли карьерного эксперта и сделай описание профессии "%s". Описание должно быть написано простым языком, для людей, которые могут не знать об этой профессии. Напиши подробное описание не больше 2 абзацев. Количество символов не больше 1000`, name)
 	descr, err = gpt.SendRequestToGPT(question)
 	if descr == "" {
 		return "", errors.New(fmt.Sprintf("Пустое описание для профессии: %s", name))
